4e69b1b4c57f0bb1df5bba5d5f04fb59d64decdc
Junya Kosaka <kosakajunya@gmail.com>
2022-02-10 11:20:43 +0900

    ...

--- utils/slist.c
+++ utils/slist.c
@@ -6,7 +6,7 @@
 /*   By: jkosaka <jkosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 18:36:06 by jkosaka           #+#    #+#             */
-/*   Updated: 2022/02/10 11:18:56 by jkosaka          ###   ########.fr       */
+/*   Updated: 2022/02/10 11:20:41 by jkosaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,6 +76,7 @@ int	slist_size(t_slist *lst)
 	while (lst->next)
 	{
 		printf("!!!!78\n");
+		printf("%s\n", lst->content);
 		ret++;
 		lst = lst->next;
 	}
