023a8b2e3c5e3eaa7bf59e8e6ec3590692b4a5d4
Junya Kosaka <kosakajunya@gmail.com>
2022-02-10 15:01:29 +0900

    ...

--- src/get_file_map.c
+++ src/get_file_map.c
@@ -6,7 +6,7 @@
 /*   By: jkosaka <jkosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 18:21:02 by jkosaka           #+#    #+#             */
-/*   Updated: 2022/02/10 14:45:23 by jkosaka          ###   ########.fr       */
+/*   Updated: 2022/02/10 15:01:27 by jkosaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ t_slist	*get_file_map(int fd)
 			return (NULL);
 		slist_add_back(&file_map, new);
 		one_line = get_next_line(fd, &is_ok);
-		if (!is_ok || (one_line && column < count_words(one_line, ' '))) // 変更
+		if (!is_ok || (one_line && column != count_words(one_line, ' '))) // 変更
 		{
 			slist_clear(&file_map);
 			return (free_one_line(one_line));
