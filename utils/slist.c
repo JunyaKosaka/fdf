b0a4f65944ba108fee5ec7efa8e591697d04c659
Junya Kosaka <kosakajunya@gmail.com>
2022-02-14 23:15:11 +0900

    ...

--- utils/slist.c
+++ utils/slist.c
@@ -6,7 +6,7 @@
 /*   By: jkosaka <jkosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 18:36:06 by jkosaka           #+#    #+#             */
-/*   Updated: 2022/02/14 23:10:24 by jkosaka          ###   ########.fr       */
+/*   Updated: 2022/02/14 23:15:06 by jkosaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,7 @@ int	slist_size(t_slist *lst)
 
 	if (!lst)
 		return (0);
-	ret = 0;
+	ret = 1;
 	while (lst->next)
 	{
 		ret++;
