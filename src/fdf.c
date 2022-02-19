9114daac6629c6060eb814b01df5d7fbda0f2582
Junya Kosaka <kosakajunya@gmail.com>
2022-02-19 18:34:14 +0900

    ...

--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: jkosaka <jkosaka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 14:48:41 by jkosaka           #+#    #+#             */
-/*   Updated: 2022/02/19 18:32:43 by jkosaka          ###   ########.fr       */
+/*   Updated: 2022/02/19 18:34:08 by jkosaka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ void	init_fdf(t_fdf *fdf)
 	fdf->origin.x = 0;
 	fdf->origin.y = 0;
 	fdf->origin.z = 0;
+	get_map_size(fdf, file_map);
 }
 
 void	prepare_vecs(t_fdf *fdf, t_slist *file_map)
@@ -65,7 +66,6 @@ void	fdf(char *filename)
 	if (!file_map)
 		free_fdf(NULL, file_map, true);
 	init_fdf(&fdf);
-	get_map_size(&fdf, file_map);
 	prepare_vecs(&fdf, file_map);
 	set_vectors(&fdf, file_map);
 	slist_clear(&file_map);
